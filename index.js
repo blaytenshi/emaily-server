b5affd87cfaa5a606d53f1225e74d33768d942d5
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-02 01:53:33 +1000

    Lesson 19 - Passport Setup

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express(); // gets an instance of an express server. Usually you'll only ever need one.
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+passport.use(new GoogleStrategy()); // creates a new instance of the GoogleStrategy
 
 const PORT = process.env.PORT || 5000; // port will be set by your server or default to 5000
 app.listen(PORT);
