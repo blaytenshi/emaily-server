4fca1ddab4007f5912f335957e4c1a1cb36996db
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-02 01:36:21 +1000

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); // gets an instance of an express server. Usually you'll only ever need one.
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000; // port will be set by your server or default to 5000
