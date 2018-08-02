d7e3becbf3a2a3a8e8907e46bf98ecabddcb8c2b
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-03 02:19:24 +1000

    Lesson 36 - Saving Model Instances

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/User'); // this needs to be before we require services/passport because we need to load the model before we can use it in services/passport
 require('./services/passport'); // this ensures the passport file gets executed (even though there's no export)
-require('./models/User');
 // const authRoutes = require('./routes/authRoutes'); // exporting authRoutes to a const
 
 mongoose.connect(keys.mongoURI);
@@ -10,7 +10,7 @@ mongoose.connect(keys.mongoURI);
 const app = express(); // gets an instance of an express server. Usually you'll only ever need one.
 
 // authRoutes(app); // sets up routes
-require('./routes/authRoutes')(app); // this line replaces line 4 and line 12. This is currying.
+require('./routes/authRoutes')(app); // this line replaces line 6 and line 12. This is currying.
 // the way to look at this line is that require, once executed, exports a function. Then we're invoking that function with the 'app' const.
 
 const PORT = process.env.PORT || 5000; // port will be set by your server or default to 5000
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users'); // fetch the model class out of mongoose instead of requiring the whole thing in because reasons (mongoose doesn't like it)
 
 passport.use(
     new GoogleStrategy({
@@ -8,8 +11,11 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback' // what google will send back the OAuth code on
     }, (accessToken, refreshToken, profile, done) => { // this callback is called by the passport.authenticate() function in /auth/google/callback automatically
-        console.log('access token', accessToken); // like a permission pass (token) to go to google and make requests for information
-        console.log('refresh token', refreshToken); // a pass to refresh the access token because the access token expires after a period of time
-        console.log('profile', profile); // profile data
+        // console.log('access token', accessToken); // like a permission pass (token) to go to google and make requests for information
+        // console.log('refresh token', refreshToken); // a pass to refresh the access token because the access token expires after a period of time
+        // console.log('profile', profile); // profile data
+        new User({
+            googleId: profile.id // creates a new instance of a user
+        }).save() // saves it to mongodb
     })
 ); // creates a new instance of the GoogleStrategy
