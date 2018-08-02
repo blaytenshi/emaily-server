9b9d1872a645ccb7001bd04d9bf067f1649359f5
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-03 02:11:28 +1000

    Lesson 35 - Mongoose Model Classes

---
+++ models/User.js
@@ -0,0 +1,14 @@
+// This is a model class (as indicated by Capital in file name). Used by MongoDB.
+const mongoose = require('mongoose');
+// const Schema = mongoose.Schema; // same as line 3
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    googleId: String // what piece of data to store and what type it is
+});
+
+// tells mongoose to create a new collection called users
+mongoose.model(
+    'users', // name of the collection
+    userSchema // schema of the collection
+);
