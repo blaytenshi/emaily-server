959f322aa4f9f2ab6cd9be6f44cd0610c5b41846
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-17 15:29:43 +1000

    Lesson 58 - Refactoring with Async/Await

--- services/passport.js
+++ services/passport.js
@@ -24,22 +24,17 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback', // what google will send back the OAuth code on, this relative path may cause the app to switch from https to http thereby causing the OAuth flow to break.
         proxy: true // we need this option to because when Google strategy goes through a proxy, it automatically does NOT trust information coming back from a proxy and thus drops the S from HTTPS.
-    }, (accessToken, refreshToken, profile, done) => { // this callback is called by the passport.authenticate() function in /auth/google/callback automatically
-        // console.log('access token', accessToken); // like a permission pass (token) to go to google and make requests for information
-        // console.log('refresh token', refreshToken); // a pass to refresh the access token because the access token expires after a period of time
-        // console.log('profile', profile); // profile data
-        User.findOne({ googleId: profile.id }) // find the first matching key (googleId) with a given value (profile.id) and return it
-            .then((existingUser) => { // when the promise resolves it will either contain the existing user or it will return null (error first convention)
-                if (existingUser) {
-                    // we already have a record with the given profile ID
-                    done(null, existingUser); // calling done() function to tell passport that we're complete and proceed with the auth flow. First param is an error object. Second param is the user record.
-                } else {
-                    // we don't have a user record with this ID, make a new record
-                    new User({googleId: profile.id}) // creates a new instance of a user
-                        .save() // saves it to mongodb
-                        .then(user => done(null, user)); // save is an async call too so we're going to wait for it to come back and trigger the then() with the created user from mongo's side
-                }
-            })
+    },
+    async (accessToken, refreshToken, profile, done) => { // this callback is called by the passport.authenticate() function in /auth/google/callback automatically
+        const existingUser = await User.findOne({ googleId: profile.id }); // find the first matching key (googleId) with a given value (profile.id) and return it. The returned object will either be the existing user or null.
 
+        if (existingUser) {
+            // we already have a record with the given profile ID
+            return done(null, existingUser); // calling done() function to tell passport that we're complete and proceed with the auth flow. First param is an error object. Second param is the user record. Calling return here will ensure the rest of the code is not executed.
+        }
+
+        // we don't have a user record with this ID, make a new record
+        const user = await new User({googleId: profile.id}).save(); // creates a new instance of a user and saves it to mongodb, await for it to complete and whatever comes back, assign to user variable
+        done(null, user);
     })
 ); // creates a new instance of the GoogleStrategy
