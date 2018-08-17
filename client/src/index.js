e401f978d19fee50a7de01c652403fbb83eea113
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-17 15:47:17 +1000

    Lesson 60 - Client React Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
+// Data layer control (Redux)
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDom.render();
