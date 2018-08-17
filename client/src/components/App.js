c4964f34e0cfeb61927d3c43e25878bbf59f0d9e
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-17 16:51:41 +1000

    Lesson 67 - React Router Setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,11 @@
 // Rendering layer control (React Router)
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return (
