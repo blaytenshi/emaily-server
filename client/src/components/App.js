e5cfb5ace604b69fe6217050ed3ef88a839c1604
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-20 10:58:27 +1000

    Lesson 77 - Refactoring the App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,24 +4,34 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
 
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-    return (
-        <div className="container">
-            <BrowserRouter>
-                <div>
-                    <Header // will now show at all times
-                    />
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/surveys" component={Dashboard} />
-                    <Route path="/surveys/new" component={SurveyNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+class App extends React.Component {
+
+    // componentDidMount looks like it might be the place to do async requests in the future
+    // replacing componentWillMount... WillMount might be called multiple times. Design changes to
+    // React.
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header // will now show at all times
+                        />
+                        <Route exact path="/" component={Landing}/>
+                        <Route exact path="/surveys" component={Dashboard}/>
+                        <Route path="/surveys/new" component={SurveyNew}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 };
 
 export default App;
