cc6cf41f1adafdf91503d893d6a2811098330ce1
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-20 14:12:10 +1000

    Lesson 85 - Landing Component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,11 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
-
+import Landing from './Landing';
 import Header from './Header';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends React.Component {
 
