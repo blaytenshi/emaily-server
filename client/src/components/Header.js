6113ebe8e48771047700f00f031945648507d1ab
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-20 15:35:15 +1000

    Lesson 94 - Stripe Tokens

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
+import Payments from './Payments';
 
 class Header extends Component {
 
@@ -11,7 +13,10 @@ class Header extends Component {
             case false:
                 return <li><a href="/auth/google">Login With Google</a></li>;
             default:
-                return <li><a href="/api/logout">Logout</a></li>;
+                return [
+                    <li><Payments/></li>,
+                    <li><a href="/api/logout">Logout</a></li>
+                ];
         }
     }
 
---
+++ client/src/components/Payments.js
@@ -0,0 +1,18 @@
+// Stripe wrapper
+// Renders a Payment Button
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends React.Component {
+    render() {
+        return (
+            <StripeCheckout
+                amount={500} // defaults to USD, measured in cents
+                token={token => console.log(token)} // expects a callback function that will be called when we recieve a token back from stripe
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
+            />
+        )
+    }
+}
+
+export default Payments;
