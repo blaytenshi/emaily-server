9dee7015764f88ed36bbbb9dc24aa975485423db
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-20 10:53:34 +1000

    Lesson 76 - Basics of Redux Thunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,5 +2,29 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 const fetchUser = () => {
-    axios.get('/api/current_user');
+    // What we would usually have without redux thunk:
+    // Make the request...
+    // const request = axios.get('/api/current_user');
+
+    // return an action object with the data inside
+    // return {
+    //     type: FETCH_USER,
+    //     payload: request
+    // }
+
+    // Instead we'll have this:
+    // We are not returning an action anymore, we're returning a function.
+    // The reason for this is that after we wired up the redux-thunk middleware,
+    // redux-thunk will look at what is returned from an action creator.
+    // If it's a function it will pass in the dispatch function into the returned
+    // functions parameters.
+    // This allows us to dispatch (or send) an action whenever we want and not IMMEDIATELY
+    // if we didn't have redux-thunk.
+    // So why do we want to work with redux-thunk right here? well the axios.get is an async
+    // function. We have to wait for all the data to come back after the request and THEN
+    // dispatch the action with all the data in it.
+    return function(dispatch) {
+        axios.get('/api/current_user')
+            .then(res => dispatch({ type: FETCH_USER, payload: res }));
+    }
 };
