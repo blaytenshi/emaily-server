d022df28943a3ff80270414ce58aecc55cba5b24
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-20 16:35:13 +1000

    Lesson 102 - Finalizing a Charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,16 +2,17 @@ const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = app => {
-    app.post('/api/stripe', (req, res) => {
+    app.post('/api/stripe', async (req, res) => {
         // here we will make a request (with the token provided in request) to stripe API to check whether the payment was finalised
         // once it's finalised we will increment the user's email campaign credits
         // console.log("CARD TOKEN", req.body);
-        stripe.charges.create({
-            amount: 500,
-            currency: 'usd',
+        const charge = await stripe.charges.create({
+            amount: 500, // used to confirm the amount. This value on front end is more like an authorization
+            currency: 'usd', // currency
             description: '$5 for 5 credits on Emaily',
-            source: req.body.id
-        })
+            source: req.body.id // the id of the token
+        });
 
+        console.log(charge);
     });
 };
