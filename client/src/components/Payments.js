c7c3949f022f03f7fc801553817acd749cd830bb
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-20 15:39:35 +1000

    Lesson 95 - Payment Fixes

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -10,7 +10,11 @@ class Payments extends React.Component {
                 amount={500} // defaults to USD, measured in cents
                 token={token => console.log(token)} // expects a callback function that will be called when we recieve a token back from stripe
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+                name="Emaily"
+                description="$5 for 5 email credits"
+            >
+                <button className="btn">Add Credits</button>
+            </StripeCheckout>
         )
     }
 }
