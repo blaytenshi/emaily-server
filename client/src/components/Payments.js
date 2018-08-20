2f2547ca9241e4b2c2804edbeee7a79e58eb134d
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-20 15:48:55 +1000

    Lesson 97 - Posting the Stripe Token

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -2,13 +2,15 @@
 // Renders a Payment Button
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends React.Component {
     render() {
         return (
             <StripeCheckout
                 amount={500} // defaults to USD, measured in cents
-                token={token => console.log(token)} // expects a callback function that will be called when we recieve a token back from stripe
+                token={token => this.props.handleToken(token)} // expects a callback function that will be called when we recieve a token back from stripe
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
                 name="Emaily"
                 description="$5 for 5 email credits"
@@ -19,4 +21,7 @@ class Payments extends React.Component {
     }
 }
 
-export default Payments;
+export default connect(
+    null,
+    actions
+)(Payments);
