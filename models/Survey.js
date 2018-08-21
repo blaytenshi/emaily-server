54f5abc8681eb60de46e9afc3a35472b7577b16a
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-21 10:58:11 +1000

    Lesson 118 - Setting up SubDocs

--- models/Survey.js
+++ models/Survey.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
     title: String,
     subject: String,
     body: String,
-    recipients: [String], // array of strings
+    recipients: [RecipientSchema], // array of sub document collection
     yes: { type: Number, default: 0 },
     no: { type: Number, default: 0 }
 });
