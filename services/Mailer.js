8aa8454e369a6e3876d6ee2a2c086e36cf0c0a02
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-21 16:24:44 +1000

    Lesosn 131 - Boilerplate for Sending Emails

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,10 +10,30 @@ class Mailer extends helper.Mail {
         super(); // executes any contructor on the Mail class
 
         // sendgrid stuff
-        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.from_email = new helper.Email('no-reply@emaily.com'); // helper.Email is from sendgrid
         this.subject = subject;
-        this.body = new helper.Content('text/html', content);
-        this.recipients = this.formatAddresses(recipients);
+        this.body = new helper.Content('text/html', content); // helper.Content is also from sendgrid
+        this.recipients = this.formatAddresses(recipients); // formatAddress is a custom function we define ourselves
+
+        this.addContent(this.body); // this addContent is from sendgrid. We call this to tell sendgrid that this is the content we want for the body
+        this.addClickTracking(); // our own defined function
+        this.addRecipients();
+    }
+
+    formatAddresses(recipients) {
+        // map through the recipients array that has a recipient object in the form of { email: 'blah', clicked: true/false }
+        // extract the email portion out using ES6 syntax and pass it into the function
+        return recipients.map(({ email }) => {
+            return new helper.Email(email); // use sendgrid helper library to build the email object
+        })
+    }
+
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
     }
 }
 
