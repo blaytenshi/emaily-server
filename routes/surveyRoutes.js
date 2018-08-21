06ed6ce00a56027c6afa7355ec17d9f9d01f916d
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-21 13:35:03 +1000

    Lesson 123 - Creating Subdoc Collections

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -14,6 +14,10 @@ module.exports = app => {
             subject,
             body,
             // set up Recipient sub document object here
+            // split up the recipients array by ','. map over each email element and return an object that looks like { email: email }
+            recipients: recipients.split(',').map(email => ({ email })),
+            _user: req.user.id,
+            dateSent: Date.now()
         })
     });
 };
