9767826d60558de7ce7413409cff08c7578b4e34
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-21 16:16:25 +1000

    Lesson 130 - Mailer Constructor

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,7 +3,18 @@ const helper = sendgrid.mail; // taking the mail helper from sendgrid
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+    constructor(
+        { subject, recipients }, // the desconstructed survey object
+        content // the html template
+    ) {
+        super(); // executes any contructor on the Mail class
 
+        // sendgrid stuff
+        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+    }
 }
 
 // Capital because it exports a class
