3c24a3e6812312038be2532b5292b24babee5521
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-21 16:10:57 +1000

    Lesson 129 - Mailer in Use

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -18,6 +20,9 @@ module.exports = app => {
             recipients: recipients.split(',').map(email => ({ email: email.trim() })),
             _user: req.user.id,
             dateSent: Date.now()
-        })
+        });
+
+        // send the email!
+        const mailer = new Mailer(survey, surveyTemplate(survey));
     });
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -1,8 +1,10 @@
-// Capital because it exports a class
 const sendgrid = require('sendgrid');
 const helper = sendgrid.mail; // taking the mail helper from sendgrid
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
-    
+
 }
+
+// Capital because it exports a class
+module.exports = Mailer;
