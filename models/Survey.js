0f7f4c0654b2bc68df74c2078ea6a7111a2eb1f0
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-21 10:50:29 +1000

    Lesson 116 - Model Deficiencies

--- models/Survey.js
+++ models/Survey.js
@@ -6,7 +6,9 @@ const surveySchema = new Schema({
     title: String,
     subject: String,
     body: String,
-    recipients: [String] // array of strings
+    recipients: [String], // array of strings
+    yes: { type: Number, default: 0 },
+    no: { type: Number, default: 0 }
 });
 
 mongoose.model('surveys', surveySchema);
