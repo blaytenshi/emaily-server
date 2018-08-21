310dd9425e6fbedf0aef83c9bb56b0c4edcd28b7
Jimmy Tang <blay.tenshi@gmail.com>
2018-08-21 12:44:56 +1000

    Lesson 122 - Creating Surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,19 @@
-import requireLogin from '../middlewares/requireLogin';
-import requireCredits from '../middlewares/requireCredits';
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-        
+        // these properties come from the body-parser
+        const { title, subject, body, recipients } = req.body;
+
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            // set up Recipient sub document object here
+        })
     });
 };
